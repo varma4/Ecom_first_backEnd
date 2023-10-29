61ee39eef43ab8faf2e5d630fb71d8bdaac4e884
Varma <mrvarma85@gmail.com>
2023-10-29 13:23:53 +0530

    cors-err

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = 4040
 require('dotenv').config()
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://sellshit.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();

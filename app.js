e33fc6f7ff21a695ce7105b4cf44187a7cda3c2f
Varma <mrvarma85@gmail.com>
2023-11-09 21:51:30 +0530

    Removing additional cors

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const port = 4040
 
 require('dotenv').config()
 app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://sellstuffonline.netlify.app'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://sellstuffonline.netlify.app'); 
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
 mongoose.connect(process.env.URL, console.log('DB connected'))
 
 
